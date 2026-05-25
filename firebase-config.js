d19c8101725f5b05b22a31fce1bdf223d25ee108
nonox-rgb <nnxspam@gmail.com>
2026-05-25 10:13:51 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // ⚠️ REMPLACE CES VALEURS PAR TA CONFIG FIREBASE
 // Firebase Console → Ton projet → Paramètres → Tes applications → Config SDK
 export const FIREBASE_CONFIG = {
-  apiKey: "REMPLACE_MOI",
-  authDomain: "REMPLACE_MOI.firebaseapp.com",
-  projectId: "REMPLACE_MOI",
-  storageBucket: "REMPLACE_MOI.appspot.com",
-  messagingSenderId: "REMPLACE_MOI",
-  appId: "REMPLACE_MOI"
+  apiKey: "AIzaSyD4fbQD0AhvUOUm15796-jCMRFmIjHPEyM",
+  authDomain: "velocom-ca56c.firebaseapp.com",
+  projectId: "velocom-ca56c",
+  storageBucket: "velocom-ca56c.firebasestorage.app",
+  messagingSenderId: "81315402071",
+  appId: "1:81315402071:web:96e3aecb2bac89b1286e12"
 };
