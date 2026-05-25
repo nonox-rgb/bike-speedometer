58bd882823f3ddef719b13d44caa429f95e4b143
nonox-rgb <nnxspam@gmail.com>
2026-05-25 10:12:23 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// ⚠️ REMPLACE CES VALEURS PAR TA CONFIG FIREBASE
+// Firebase Console → Ton projet → Paramètres → Tes applications → Config SDK
+export const FIREBASE_CONFIG = {
+  apiKey: "REMPLACE_MOI",
+  authDomain: "REMPLACE_MOI.firebaseapp.com",
+  projectId: "REMPLACE_MOI",
+  storageBucket: "REMPLACE_MOI.appspot.com",
+  messagingSenderId: "REMPLACE_MOI",
+  appId: "REMPLACE_MOI"
+};
